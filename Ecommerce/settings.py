d17bfd41d2944e3d07e286defbe2a40e6ab74be0
ajayjangid17 <ajayjangid178@gmail.com>
2021-07-11 16:34:28 +0530

    settings update

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Ecom',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
